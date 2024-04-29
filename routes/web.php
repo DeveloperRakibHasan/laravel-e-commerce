ee4081017b00aee87ddff1d341025e22d40007e2
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-04-29 19:27:31 +0600

    add: product details page create

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use App\Livewire\HomePage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\ProductsPage;
 use App\Livewire\CartPage;
+use App\Livewire\ProductDetailPage;
 
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/cart', CartPage::class);
+Route::get('/products/{slug}', ProductDetailPage::class);
