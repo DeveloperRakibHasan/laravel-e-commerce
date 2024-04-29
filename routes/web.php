b11814ee25870f8dbea995c45ad2d8bce65cf173
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-04-29 19:20:14 +0600

    add: card page design

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\HomePage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\ProductsPage;
+use App\Livewire\CartPage;
 
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
