1f6e210583869ccbab04cec35f60364f5cb67571
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-04-29 19:17:34 +0600

    add: product page create

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\HomePage;
 use App\Livewire\CategoriesPage;
+use App\Livewire\ProductsPage;
 
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductsPage::class);
