a1f246a74ed1f096051411d6b892a868eef9931f
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-04-29 18:44:27 +0600

    fix: category page category add

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\HomePage;
+use App\Livewire\CategoriesPage;
 
 Route::get('/', HomePage::class);
+Route::get('/categories', CategoriesPage::class);
