6e1fd06b98120c3785c7c90a77e28bad960ed4ab
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-04-30 20:18:59 +0600

    add file

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use App\Models\Product;
+use Livewire\WithPagination;
 
+#[Title('Products - Tech Store')]
 class ProductsPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1);
+//        dd($productQuery);
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(6)
+        ]);
     }
 }
