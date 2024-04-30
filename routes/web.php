cfb97f6e9f1de60499416383f227e0e402147262
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-04-30 11:13:52 +0600

    add: checkout page create

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ use App\Livewire\CategoriesPage;
 use App\Livewire\ProductsPage;
 use App\Livewire\CartPage;
 use App\Livewire\ProductDetailPage;
+use App\Livewire\CheckoutPage;
+
 
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/cart', CartPage::class);
 Route::get('/products/{slug}', ProductDetailPage::class);
+Route::get('/checkout', CheckoutPage::class);
