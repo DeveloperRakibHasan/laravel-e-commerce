d42bd90541a73ea18182d5e5496693370f6fd23c
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-05-02 15:04:08 +0600

    dark theme text color set

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -27,7 +27,7 @@
                                 <li class="mb-4" wire:key="{{ $brand->id }}">
                                     <label for="{{$brand->slug}}" class="flex items-center dark:text-gray-400 ">
                                         <input type="checkbox" id="{{$brand->slug}}" value="{{$brand->id}}" class="w-4 h-4 mr-2">
-                                        <span class="text-lg">{{$brand->name}}</span>
+                                        <span class="text-lg dark:text-gray-400">{{$brand->name}}</span>
                                     </label>
                                 </li>
                             @endforeach
