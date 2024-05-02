11a12a95ecc26dab11d8c4043cf09033ea651f5d
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-05-02 18:01:12 +0600

    products category filter set

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Brand;
 use App\Models\Category;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use App\Models\Product;
 use Livewire\WithPagination;
@@ -13,10 +14,17 @@ use Livewire\WithPagination;
 class ProductsPage extends Component
 {
     use WithPagination;
+
+    #[Url]
+    public $selected_categories = [];
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
-//        dd($productQuery);
+
+        if(!empty($this->selected_categories)){
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+
         return view('livewire.products-page', [
             'products' => $productQuery->paginate(6),
             'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
