8f66d41261eda540a5cf896fce1f45e22ec99fd7
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-05-02 15:54:55 +0600

    product details page data dynamic set

--- resources/views/livewire/product-detail-page.blade.php
+++ resources/views/livewire/product-detail-page.blade.php
@@ -2,7 +2,7 @@
     <section class="overflow-hidden bg-white py-11 font-poppins dark:bg-gray-800">
         <div class="max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
             <div class="flex flex-wrap -mx-4">
-                <div class="w-full mb-8 md:w-1/2 md:mb-0" x-data="{ mainImage: 'https://m.media-amazon.com/images/I/71f5Eu5lJSL._SX679_.jpg' }">
+                <div class="w-full mb-8 md:w-1/2 md:mb-0" x-data="{ mainImage: '{{url('storage', $product->images[0] ?? 'img/a.avif')}}' }">
                     <div class="sticky top-0 z-50 overflow-hidden ">
                         <div class="relative mb-6 lg:mb-10 lg:h-2/4 ">
                             <img x-bind:src="mainImage" alt="" class="object-cover w-full lg:h-full ">
@@ -36,17 +36,14 @@
                 </div>
                 <div class="w-full px-4 md:w-1/2 ">
                     <div class="lg:pl-20">
-                        <div class="mb-8 ">
+                        <div class="mb-8">
                             <h2 class="max-w-xl mb-6 text-2xl font-bold dark:text-gray-400 md:text-4xl">
-                                Macbook Pro M130c90</h2>
+                                {{ $product->name }}</h2>
                             <p class="inline-block mb-6 text-4xl font-bold text-gray-700 dark:text-gray-400 ">
-                                <span>$1500.99</span>
-                                <span class="text-base font-normal text-gray-500 line-through dark:text-gray-400">$1800.99</span>
+                                <span>{{Number::currency($product->price)}}</span>
                             </p>
                             <p class="max-w-md text-gray-700 dark:text-gray-400">
-                                Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet
-                                Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet Lorem ispum dor amet
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta eligendi esse laboriosam ex hic magni inventore dolores numquam, veniam totam ratione iusto explicabo accusamus, sit alias, architecto voluptate officiis quaerat?
+                                {!! Str::markdown($product->description) !!}
                             </p>
                         </div>
                         <div class="w-32 mb-8 ">
