b9ea1b6cab96fd17c35f7a94cb8261bf4d1ce78e
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-05-04 16:01:13 +0600

    add: remove checkout product from cart method add

--- app/Livewire/CartPage.php
+++ app/Livewire/CartPage.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Illuminate\Support\Facades\Cookie;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Cart - Tech Store')]
 class CartPage extends Component
 {
+    public $cart_items = [];
+    public $grand_total;
+
+    public function mount()
+    {
+        $this->cart_items = CartManagement::getCartItemsFromCookie();
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function removeItem($product_id)
+    {
+       $this->cart_items = CartManagement::removeCartItems($product_id);
+       $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
     public function render()
     {
         return view('livewire.cart-page');
