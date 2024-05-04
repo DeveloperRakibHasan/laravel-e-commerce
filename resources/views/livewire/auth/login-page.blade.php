9dee4107f34fc0c47b63bd6b7f6102e71522e840
DeveloperRakibHasan <mdrakibhasan240@gmail.com>
2024-05-04 18:08:47 +0600

    login page invalidate credential message set

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -17,6 +17,13 @@
 
                     <!-- Form -->
                     <form wire:submit.prevent="save">
+
+{{--                        @if(session('error'))--}}
+{{--                            <div class="bg-red-600 text-sm text-gray-500 rounded-lg p-4" role="alert">--}}
+{{--                                {{session('error')}}--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
+
                         <div class="grid gap-y-4">
                             <!-- Form Group -->
                             <div>
